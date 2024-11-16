5d05504da5d40e71cbdc3d34dd72a2240cc8d5f2
developeeer-level1 <yak.yakovochka@gmail.com>
2024-11-16 23:32:19 +0200

    Damn, i made this task

--- superPuperDz/superPuperDz.cpp
+++ superPuperDz/superPuperDz.cpp
@@ -2,7 +2,22 @@
 
 using namespace std;
 
+class Car {
+    int speed;
+public:
+    Car() {
+        speed = 999;
+        cout << "Damn, this car is freakin fast";
+    }
+
+    void PrintGeniusInfo() {
+        cout << "This car has speed: " << speed;
+    }
+
+};
+
 int main()
 {
-    cout << "Hello World!\n";
+    Car car;
+    car.PrintGeniusInfo();
 }
